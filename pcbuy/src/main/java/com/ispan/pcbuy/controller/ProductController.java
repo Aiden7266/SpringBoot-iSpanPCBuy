73363139eef958f9d44df7e25d0ee9a27c0daa1b
Aiden <kircyo87916@gmail.com>
2023-02-06 02:01:41 +0800

    商品功能 - 實作修改商品

--- pcbuy/src/main/java/com/ispan/pcbuy/controller/ProductController.java
+++ pcbuy/src/main/java/com/ispan/pcbuy/controller/ProductController.java
@@ -33,4 +33,18 @@ public class ProductController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
+
+    @PutMapping("/products/{productId}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Integer productId,
+                                                 @RequestBody @Valid ProductRequest productRequest){
+        Product product = productService.getProductById(productId);
+
+        if(product != null) {
+            productService.updateProduct(productId, productRequest);
+            Product updatedProduct = productService.getProductById(productId);
+            return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }
