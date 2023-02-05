86537c73a628f6e1421c4c52d3fb9ee2cd6f1eff
Aiden <kircyo87916@gmail.com>
2023-02-06 04:32:17 +0800

    商品功能 - 實作商品查詢列表(分頁)

--- pcbuy/src/main/java/com/ispan/pcbuy/controller/ProductController.java
+++ pcbuy/src/main/java/com/ispan/pcbuy/controller/ProductController.java
@@ -6,6 +6,8 @@ import com.ispan.pcbuy.dto.ProductRequest;
 import com.ispan.pcbuy.model.Product;
 import com.ispan.pcbuy.service.ProductService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Valid
 @RestController
 public class ProductController {
 
@@ -27,13 +30,19 @@ public class ProductController {
 
             //  排序 Sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc")         String sort,
+
+            //分頁 (要加@Valid註解才能使用@Min@Max)
+            @RequestParam(defaultValue = "5") @Min(0) @Max(100) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0)           Integer offset
     ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
